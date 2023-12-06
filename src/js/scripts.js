8ad4a437cae2e8471a979da7d314de33546394e6
arianneduenas <144288105+arianneduenas@users.noreply.github.com>
2023-12-06 07:45:48 -0500

    Javascript for quiz

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,26 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+
+document.body.addEventListener('change', function (e) {
+    let target_id = e.target.id;
+    let output = document.getElementById("output");
+    let output2 = document.getElementById("output2");
+    output.innerHTML = "";
+    output2.innerHTML = "";
+    switch (target_id) {
+        case "attend-yes":
+            output.innerHTML = '<span class="error">Oops! Try again!</span>';
+            break;
+        case "attend-no":
+            output.innerHTML = '<span class="success">Hooray! That’s right!🌟</span>';
+            break;
+        case "catch-yes":
+            output2.innerHTML = '<span class="success">Hooray! That’s right!🌟</span>';
+            break;
+        case "catch-no":
+            output2.innerHTML = '<span class="error">Oops! Try again!</span>';
+            break;
+    }
+})
