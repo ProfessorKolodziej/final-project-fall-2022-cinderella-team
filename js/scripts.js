584840d6f5b35a35562843a7f059d6d93c9e7e62
arianneduenas <144288105+arianneduenas@users.noreply.github.com>
2023-12-06 17:00:10 -0500

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -7,43 +7,45 @@
 // - You'll need to link this file to your HTML :)
 
 
-document.body.addEventListener('change', function (e) {
-    let target_id = e.target.id;
-    let output = document.getElementById("output");
-    let output2 = document.getElementById("output2");
-    output.innerHTML = "";
-    output2.innerHTML = "";
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.addEventListener('change', function (e) {
+        let target_id = e.target.id;
+        let output = document.getElementById("output");
+        let output2 = document.getElementById("output2");
+        output.innerHTML = "";
+        output2.innerHTML = "";
 
-    const wrongMessage = '<span class="error">Oops! That’s not quite right. Try again!</span>';
-    const correctMessage = '<span class="success">Well done! That’s correct!</span>';
+        const wrongMessage = '<span class="error">Oops! That’s not quite right. Try again!</span>';
+        const correctMessage = '<span class="success">Well done! That’s correct!</span>';
 
-    switch (target_id) {
-        case "attend-yes":
-            output.innerHTML = wrongMessage;
-            break;
-        case "attend-no":
-            output.innerHTML = correctMessage;
-            break;
-        case "catch-yes":
-            output2.innerHTML = wrongMessage;
-            break;
-        case "catch-no":
-            output2.innerHTML = correctMessage;
-            break;
-    }
-});
+        switch (target_id) {
+            case "attend-yes":
+                output.innerHTML = wrongMessage;
+                break;
+            case "attend-no":
+                output.innerHTML = correctMessage;
+                break;
+            case "catch-yes":
+                output2.innerHTML = wrongMessage;
+                break;
+            case "catch-no":
+                output2.innerHTML = correctMessage;
+                break;
+        }
+    });
 
-document.getElementById('submit-items').addEventListener('click', function() {
-    let correctItems = ['pumpkin', 'lizards', 'mice'];
-    let selectedItems = document.querySelectorAll('input[name="item"]:checked');
-    let selectedValues = Array.from(selectedItems).map(item => item.value);
-    let correctCount = selectedValues.filter(value => correctItems.includes(value)).length;
+    document.getElementById('submit-items').addEventListener('click', function() {
+        let correctItems = ['pumpkin', 'lizards', 'mice'];
+        let selectedItems = document.querySelectorAll('input[name="item"]:checked');
+        let selectedValues = Array.from(selectedItems).map(item => item.value);
+        let correctCount = selectedValues.filter(value => correctItems.includes(value)).length;
 
-    let feedbackElement = document.getElementById("items-feedback");
+        let feedbackElement = document.getElementById("items-feedback");
 
-    if (correctCount === correctItems.length && correctCount === selectedItems.length) {
-        feedbackElement.innerHTML = '<span class="success">You got it right! Well done!</span>';
-    } else {
-        feedbackElement.innerHTML = '<span class="error">Nice try! But some answers are not quite right.</span>';
-    }
+        if (correctCount === correctItems.length && correctCount === selectedItems.length) {
+            feedbackElement.innerHTML = '<span class="success">You got it right! Well done!</span>';
+        } else {
+            feedbackElement.innerHTML = '<span class="error">Nice try! But some answers are not quite right.</span>';
+        }
+    });
 });
