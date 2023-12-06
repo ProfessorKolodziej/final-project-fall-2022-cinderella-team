120479ab648f41109eab91907b6b3f98830444e2
arianneduenas <144288105+arianneduenas@users.noreply.github.com>
2023-12-06 08:31:37 -0500

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,30 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+
+document.body.addEventListener('change', function (e) {
+    let target_id = e.target.id;
+    let output = document.getElementById("output");
+    let output2 = document.getElementById("output2");
+    output.innerHTML = "";
+    output2.innerHTML = "";
+
+    const wrongMessage = '<span class="error">Oops! That’s not quite right. Try again!</span>';
+    const correctMessage = '<span class="success">Well done! That’s correct!</span>';
+
+    switch (target_id) {
+        case "attend-yes":
+            output.innerHTML = wrongMessage;
+            break;
+        case "attend-no":
+            output.innerHTML = correctMessage;
+            break;
+        case "catch-yes":
+            output2.innerHTML = wrongMessage;
+            break;
+        case "catch-no":
+            output2.innerHTML = correctMessage;
+            break;
+    }
+});
